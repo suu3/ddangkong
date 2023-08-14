7884975880bfb2be02e9d5c8c93e283ed0317a56
suu3 <ahob789@ewhain.net>
2023-08-15 08:40:39 +0900

    :sparkles: [feat] initial reset

---
+++ app/coffee/layout.tsx
@@ -0,0 +1,7 @@
+export default function CoffeeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ app/coffee/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Coffee() {
+  return <main className="">test</main>;
+}
