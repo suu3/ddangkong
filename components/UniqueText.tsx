f8f111f91d20fdc4b146a5c541986a34097391cb
suu3 <ahob789@ewhain.net>
2023-08-24 02:29:15 +0900

    :sparkles: [feat] lint, prettier

--- components/UniqueText.tsx
+++ components/UniqueText.tsx
@@ -1,28 +1,18 @@
-import clsx from "clsx";
-import styles from "./unique-text.module.css";
+import clsx from 'clsx';
+import styles from './unique-text.module.css';
 
 interface UniqueTextProps {
-  Tag: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+  Tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
   children: React.ReactNode;
-  font?: "sans" | "uhbee";
-  size?: "sm" | "md" | "lg";
+  font?: 'sans' | 'uhbee';
+  size?: 'sm' | 'md' | 'lg' | '';
   className?: string;
   style?: React.CSSProperties;
 }
 
-const UniqueText = ({
-  Tag,
-  children,
-  font = "sans",
-  size,
-  className,
-  style,
-}: UniqueTextProps) => {
+const UniqueText = ({ Tag, children, font = 'sans', size = '', className, style }: UniqueTextProps) => {
   return (
-    <Tag
-      className={clsx(styles["common"], styles[font], styles[size], className)}
-      style={style}
-    >
+    <Tag className={clsx(styles['common'], styles[font], styles[size], className)} style={style}>
       {children}
     </Tag>
   );
