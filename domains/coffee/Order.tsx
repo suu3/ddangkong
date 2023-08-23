a12a8ce44ead8aaae58ec8f7004f7ea4e3202aba
suu3 <ahob789@ewhain.net>
2023-08-24 02:43:48 +0900

    :bug: [fix] build error

--- domains/coffee/Order.tsx
+++ domains/coffee/Order.tsx
@@ -1,10 +1,10 @@
-import BubbleContainer from "@/components/BubbleContainer";
-import MainButton from "@/components/button/MainButton";
-import UpDownButton from "@/components/button/UpDownButton";
-import UniqueText from "@/components/UniqueText";
-import useUpDownCnt from "@/lib/hooks/useUpDownCnt";
-import baristarImage from "@/public/coffee/baristar.svg";
-import Image from "next/image";
+import BubbleContainer from '@/components/BubbleContainer';
+import MainButton from '@/components/button/MainButton';
+import UpDownButton from '@/components/button/UpDownButton';
+import UniqueText from '@/components/UniqueText';
+import useUpDownCnt from '@/lib/hooks/useUpDownCnt';
+import baristarImage from '@/public/coffee/baristar.svg';
+import Image from 'next/image';
 
 export default function Order() {
   const [totalCnt, handleTotalIncrease, handleTotalDecrease] = useUpDownCnt(0);
@@ -26,37 +26,17 @@ export default function Order() {
       />
       <div className="mb-16">
         <div className="flex items-center justify-center">
-          <UniqueText
-            Tag="span"
-            size="md"
-            font="sans"
-            className="mr-4"
-            style={{ width: "4.35rem" }}
-          >
+          <UniqueText Tag="span" size="md" font="sans" className="mr-4" style={{ width: '4.35rem' }}>
             총 인원 :
           </UniqueText>
-          <UpDownButton
-            handleIncrease={handleTotalIncrease}
-            handleDecrease={handleTotalDecrease}
-            count={totalCnt}
-          />
+          <UpDownButton handleIncrease={handleTotalIncrease} handleDecrease={handleTotalDecrease} count={totalCnt} />
         </div>
 
         <div className="flex items-center justify-center pt-4">
-          <UniqueText
-            Tag="span"
-            size="md"
-            font="sans"
-            className="mr-4 text-right"
-            style={{ width: "4.35rem" }}
-          >
+          <UniqueText Tag="span" size="md" font="sans" className="mr-4 text-right" style={{ width: '4.35rem' }}>
             꽝 :
           </UniqueText>
-          <UpDownButton
-            handleIncrease={handleBlankIncrease}
-            handleDecrease={handleBlankDecrease}
-            count={blankCnt}
-          />
+          <UpDownButton handleIncrease={handleBlankIncrease} handleDecrease={handleBlankDecrease} count={blankCnt} />
         </div>
       </div>
       <MainButton label="주문하기" variant="contained" color="chocolate" />
--- lib/hooks/useUpDownCnt.ts
+++ lib/hooks/useUpDownCnt.ts
@@ -1,18 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-interface UseUpDownCnt {
-  initialValue?: number;
-}
-
-const useUpDownCnt = ({ initialValue = 0 }: UseUpDownCnt) => {
+const useUpDownCnt = (initialValue: number = 0) => {
   const [count, setCount] = useState(initialValue);
 
   const handleIncrease = () => {
-    setCount((prev) => prev + 1);
+    setCount(prev => prev + 1);
   };
 
   const handleDecrease = () => {
-    setCount((prev) => prev - 1);
+    setCount(prev => prev - 1);
   };
 
   return [count, handleIncrease, handleDecrease] as const;
