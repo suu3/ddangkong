f62250d5f7b947d1bd6d7f01ce2e8f33ab5a6c53
suu3 <ahob789@ewhain.net>
2023-08-24 04:12:30 +0900

    :sparkles: [feat] shuffle 퍼블리싱 (애니메이션 X)

--- app/coffee/page.tsx
+++ app/coffee/page.tsx
@@ -1,15 +1,17 @@
-"use client";
+'use client';
 
-import Order from "@/domains/coffee/Order";
-import Start from "@/domains/coffee/Start";
-import { useState } from "react";
+import Order from '@/domains/coffee/Order';
+import Shuffle from '@/domains/coffee/Shuffle';
+import Start from '@/domains/coffee/Start';
+import { useState } from 'react';
 
 export default function Coffee() {
   const [step, setStep] = useState(0);
   return (
     <div>
       {/* <Start /> */}
-      <Order />
+      {/* <Order /> */}
+      <Shuffle />
     </div>
   );
 }
--- components/button/MainButton.tsx
+++ components/button/MainButton.tsx
@@ -1,25 +1,15 @@
-import clsx from "clsx";
-import styles from "./main-button.module.css";
+import clsx from 'clsx';
+import styles from './main-button.module.css';
 
 interface MainButtonProps {
   label: string;
-  variant?: "contained"; //"outlined" |
-  color?: "chocolate";
+  variant?: 'outlined' | 'contained';
+  color?: 'chocolate';
   className?: string;
 }
 
-const MainButton = ({
-  label,
-  variant = "contained",
-  color = "chocolate",
-  className,
-}: MainButtonProps) => {
-  const btnCls = clsx(
-    styles["button"],
-    styles[variant],
-    styles[color],
-    className
-  );
+const MainButton = ({ label, variant = 'contained', color = 'chocolate', className }: MainButtonProps) => {
+  const btnCls = clsx(styles['button'], styles[variant], styles[color], className);
   return <button className={btnCls}>{label}</button>;
 };
 
