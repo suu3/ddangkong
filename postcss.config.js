97e902ee87fc6014d1e82485bd7d33428ee26a91
suu3 <dev.suu3@gmail.com>
2026-03-18 04:56:45 +0900

    Change tailwindcss/nesting plugin configuration from a string to an empty object.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    'tailwindcss/nesting': 'postcss-nesting',
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
     'postcss-preset-env': {
